d17bc70aa8102893f8278c46e350f22cb6dc9857
scriptedlantern <robloxfunder228@gmail.com>
2026-08-28 18:53:44 -0700

    Add Blatant module registry for v2.13

---
+++ modules/Blatant/init.lua
@@ -0,0 +1,14 @@
+-- LanternVape V3 - Blatant module registry
+-- The GUI connector can use this registry to discover Blatant modules.
+
+local Registry = {
+    "Speed",
+    "Spider",
+    "Phase",
+    "Antifall",
+    "Fly",
+    "HighJump",
+    "InfiniteJump",
+}
+
+return Registry
